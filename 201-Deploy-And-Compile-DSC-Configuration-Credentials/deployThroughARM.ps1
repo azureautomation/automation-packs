6f6c1c528b62b158541fd58c0f58dc3bda229b28
bdanse <bartdanse@hotmail.com>
2015-10-24 22:37:13 +0200

    fixed configuration data

--- 201-Deploy-And-Compile-DSC-Configuration-Credentials/deployThroughARM.ps1
+++ 201-Deploy-And-Compile-DSC-Configuration-Credentials/deployThroughARM.ps1
@@ -1,6 +1,6 @@
 
 #Connect to your Azure account
-Add-AzureAccount
+#Add-AzureAccount
 
 #Select your subscription if you have more than one
 #Select-AzureSubscription -SubscriptionName "My Subscription Name"
@@ -13,7 +13,7 @@ $ConfigurationData = @{
     AllNodes = @(
         @{
             NodeName = "*";
-            PSDscAllowPlainTextPassword = "true"
+            PSDscAllowPlainTextPassword = $true
          },
         @{
             NodeName = "SRV001" 
@@ -24,39 +24,30 @@ $ConfigurationData = @{
    )
 }
 
-
-#"{\"AllNodes\":[{\"NodeName\":\"*\",\"PSDscAllowPlainTextPassword\":true},{\"NodeName\":\"FS002\"}]}"
-#{"AllNodes":[{"NodeName":"*","PSDscAllowPlainTextPassword":true},{"NodeName":"FS002"}]} 
-#{\"AllNodes\":[{\"NodeName\":\"*\",\"PSDscAllowPlainTextPassword\":\"true\"},{\"NodeName\":\"SRV001\"},{\"NodeName\":\"SRV002\"}]}
-
-
-#$EscapedConfigurationData = "`"" + ($ConfigurationData | ConvertTo-Json -Compress).replace("`"", "`\`"") + "`""
-$EscapedConfigurationData = ($ConfigurationData | ConvertTo-Json -Compress).replace("`"", "`\`"")
-
-
 #Use Azure resource Manager to deploy template 
 Switch-AzureMode -Name AzureResourceManager
 
 #Set the parameter values for the template
 $Params = @{
-    'accountName' = 'MyAutomationAccount'
-	'regionId' = 'East US 2'
-    'credentialName' = 'domainCreds'
-    'userName' = '"DOMAIN\Account"'
-    'password' = 'secret'
-    'variableName' = 'domainName'
-    'variableType' = 'string'
-    'variableValue' = '"DOMAIN.LOCAL"'
-    'configurationName' = 'dscDomainJoin'
-    'configurationURI' = 'https://raw.githubusercontent.com/bdanse/azure/master/201-Deploy-And-Compile-DSC-Configuration-Credentials/Configurations/dscDomainJoin.ps1'
-    'configurationDescription' = 'Configuration for Domain Join with credentials support through ConfigurationData'
-    'jobId' = $JobGUID
-    'jobConfigurationData' = $EscapedConfigurationData
+    accountName = 'MyAutomationAccount'
+	regionId = 'East US 2'
+    credentialName = 'domainCreds'
+    userName = '"DOMAIN\Account"'
+    password = 'secret'
+    variableName = 'domainName'
+    variableType = 'string'
+    variableValue = '"DOMAIN.LOCAL"'
+    configurationName = 'dscDomainJoin'
+    configurationURI = 'https://raw.githubusercontent.com/bdanse/azure/master/201-Deploy-And-Compile-DSC-Configuration-Credentials/Configurations/dscDomainJoin.ps1'
+    configurationDescription = 'Configuration for Domain Join with credentials support through ConfigurationData'
+    jobId = $JobGUID
+    jobConfigurationData = ($ConfigurationData | ConvertTo-Json -Compress).ToString()
 	
 }
 
-$TemplateFile = 'C:\Users\admbada\Source\Repos\azure\201-Deploy-And-Compile-DSC-Configuration-Credentials\azuredeploy.json'
+
 $TemplateURI = "https://raw.githubusercontent.com/bdanse/azure/master/201-Deploy-And-Compile-DSC-Configuration-Credentials/azuredeploy.json"
 
-#New-AzureResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "resourcgroup01" -TemplateUri $TemplateURI
-New-AzureResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "resourcgroup01" -TemplateFile $TemplateFile
+New-AzureResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "resourcgroup01" -TemplateUri $TemplateURI
+
+
