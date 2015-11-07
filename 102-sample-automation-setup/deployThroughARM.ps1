e4b6cdade21330ef97184fdb8e30344c9a03ac40
Joe Levy <jolevy@JOLEVY-LENOVO.redmond.corp.microsoft.com>
2015-11-06 17:21:05 -0800

    adding vm and vm dsc extension to sample automation account pack. dsc extension registers the vm as an aa dsc node

--- 102-sample-automation-setup/deployThroughARM.ps1
+++ 102-sample-automation-setup/deployThroughARM.ps1
@@ -4,21 +4,27 @@ Login-AzureRmAccount
 #Select your subscription if you have more than one
 #Select-AzureSubscription -SubscriptionName "My Subscription Name"
 
-$ResourceGroupName = "MySampleResourceGroup"
-$AutomationAccountName = "MySampleAutomationAccount"
+$ResourceGroupName = "SampleResourceGroup"
+$AutomationAccountName = "SampleAutomationAccount"
 $Location = "South Central US"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
 New-AzureRmAutomationAccount -ResourceGroupName $ResourceGroupName -Name $AutomationAccountName -Location $Location -Plan Free
 
+$RegistrationInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName
+
 #Set the parameter values for the template
 $Params = @{
     "accountName" = $AutomationAccountName
 	"regionId" = $Location
-    "vmName" = ("SampleVM" + [System.Guid]::NewGuid().toString().substring(0,7))
+    "registrationKey" = $RegistrationInfo.PrimaryKey
+    "registrationUrl" = $RegistrationInfo.Endpoint
+    "vmName" = ("SampleVM" + [System.Guid]::NewGuid().toString().Substring(0,7))
     "dscCompilationJobId" = [System.Guid]::NewGuid().toString()
     "runbookJobId" = [System.Guid]::NewGuid().toString()
     "jobScheduleId" = [System.Guid]::NewGuid().toString()
+    "timestamp" = (Get-Date).toString()
+    "storageAccountName" = ("SampleVMStorage" + [System.Guid]::NewGuid().toString().Substring(0,9))
 }
 
 $TemplateURI = "https://raw.githubusercontent.com/azureautomation/automation-packs/master/102-sample-automation-setup/azuredeploy.json"
