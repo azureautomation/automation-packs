fd6ef2857f93f578632462c8a1e8b24616932f21
David Tesar <david.tesar@microsoft.com>
2016-08-05 18:01:49 -0700

    1st draft

---
+++ 101-remove-resourcegroups/DeployThroughARM.ps1
@@ -0,0 +1,20 @@
+﻿
+#Connect to your Azure account
+Add-AzureRmAccount 
+
+#Select your subscription if you have more than one
+#Select-AzureRmSubscription -SubscriptionId "Your subcription ID here"
+
+#Create a GUID for the job
+$JobGUID = [System.Guid]::NewGuid().toString()
+
+#Set the parameter values for the template
+$Params = @{
+    "accountName" = "MyAccount2" ;
+    "regionId" = "Japan East";
+	"userName" = "Your UserName"; 
+	"password" = "Your password";
+}
+
+$TemplateURI = "https://raw.githubusercontent.com/azureautomation/automation-packs/master/101-remove-resourcegroups/azuredeploy.json"
+New-AzureRMResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "AzureAutomationRG" -TemplateUri $TemplateURI
