2d86e4b49deee5651437aad3b2c6b79b16524cab
eamonoreilly <eamono@microsoft.com>
2018-08-02 09:45:54 -0700

    Adding new sample on how to pass parameters to a runbook.

---
+++ 101-import-and-start-job/Runbooks/Hello-World.ps1
@@ -0,0 +1,8 @@
+﻿param  (
+        [Parameter(Mandatory=$False)]
+        $JsonParameterList = '{"Name":"World"}'
+)
+$ErrorActionPreference = 'stop'
+$Params = ConvertFrom-Json $JsonParameterList
+
+Write-Output ("Hello " + $Params.Name)
